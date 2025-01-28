d9f797de4a14b46467691d6d4a71c45ad2d0fcec
Mercedes Chase-Kelly <mercedestck@gmail.com>
2025-01-28 16:30:22 +0000

    Create Track component

--- src/components/Track/Track.jsx
+++ src/components/Track/Track.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 
-import './Track';
+import '../Track/Track.css';
 
 function Track() {
 
     return (
 
-        <Track />
+        <div className="Track">
+            <div className="Track-Info">
+                <h3>Name</h3>
+                <p>Artist | Album</p>
+            </div>
+        </div>
+  
 
     )
 
